e913a56f740026db8983ae4ae03f52cb5dae2e8f
sakut97 <sakut97@outlook.jp>
2020-10-28 18:17:59 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'posts#index'
-  get "posts/new",to: "posts#new"
   post "posts", to: "posts#create"
+  get "posts" , to: "posts#checked"  #（queryパラメーター）
 end
