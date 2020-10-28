ace34c7bf1e7a2e5fc2f83ac0944b2f6bc82de91
sakut97 <sakut97@outlook.jp>
2020-10-28 17:04:19 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,11 @@ class PostsController < ApplicationController
 
   def index #indexアクションの定義
    # @post = Post.find(1) #１番目のレコードを＠postに代入
-     @posts = Post.all
-  end
-
-  def new #newアクション＝新規投稿ページの表示を定義
+     @posts = Post.all.order(id: "DESC")
   end
 
   def create #createアクション＝データを投稿する
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
